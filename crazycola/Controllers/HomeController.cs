b14d5684e1a9650d0657ac89e3596f205c8ae9b7
Alejandro Aragon <aragondru155@gmail.com>
2023-05-28 23:13:05 -0600

    login initializing

--- crazycola/Controllers/HomeController.cs
+++ crazycola/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace crazycola.Controllers
     {
         public ActionResult Index()
         {
+            //string mensaje = "Hola desde ASP.NET";
+            ViewBag.Mensaje = Session["UsuarioId"];
             return View();
         }
 
---
+++ crazycola/Controllers/LoginController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using crazycola.Models;
+
+namespace crazycola.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Usuario objUser)
+        {
+            Console.WriteLine("ANTES DEL IFFFFFFFFFFF");
+
+                using (crazycolaEntities db = new crazycolaEntities())
+                {
+                    var obj = db.Usuario.Where(a => a.Email.Equals(objUser.Email) && a.Contrasenia.Equals(objUser.Contrasenia)).FirstOrDefault();
+                    Console.WriteLine(obj);
+                    Content("HOLA");
+                    if (obj != null)
+                    {
+                    Console.WriteLine("ENTREEEEE AL IFFFFFFFFFFFFFFFFF");
+                        Session["UsuarioId"] = obj.UsuarioId.ToString();
+                        Session["Email"] = obj.Email.ToString();
+                        return RedirectToAction("About","HomeController");
+                    }
+                
+            }
+            Console.WriteLine("SIN ENTRAR AL IFFFFFFFFFFF");
+            return View(objUser);
+        }
+    }
+}
